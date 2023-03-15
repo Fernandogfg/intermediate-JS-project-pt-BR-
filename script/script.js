327724508157a64455884358da4d06e2610450ac
Fernando <fernandofgfg10@gmail.com>
2023-03-14 20:54:23 -0400

    categorias

--- script/script.js
+++ script/script.js
@@ -13,84 +13,115 @@ let fadeDespesas = document.getElementById("fade-despesas");
 let fadeAddCategorias = document.getElementById("fade-add-categorias");
 let tabelaDespesas = document.getElementById("tabela-despesas");
 let containerLista = document.getElementById("container-lista");
+let tabelaCategorias = document.getElementById("lista-categorias");
+let listaCategorias = [
+  {
+    nome: "Alimentação",
+    id: "1010232322",
+  },
+  {
+    nome: "Lazer",
+    id: "1010232323",
+  },
+  {
+    nome: "Conta",
+    id: "1010232324",
+  },
+];
 let listaDespesas = [
   {
     dataVencimento: "18/03/2023",
     despesa: "lanche",
     valor: "127.58",
     status: true,
-    categoria: "alimentação",
+    categoria: "1010232322",
   },
   {
     dataVencimento: "01/03/2023",
     despesa: "viajem",
     valor: "17.58",
     status: false,
-    categoria: "lazer",
+    categoria: "1010232323",
   },
   {
     dataVencimento: "07/08/2022",
     despesa: "compras",
     valor: "79.2",
     status: false,
-    categoria: "alimentação",
+    categoria: "1010232322",
   },
   {
     dataVencimento: "10/10/2022",
     despesa: "cinema",
     valor: "50.0",
     status: true,
-    categoria: "lazer",
+    categoria: "1010232323",
   },
   {
     dataVencimento: "11/03/2023",
     despesa: "luz",
     valor: "150.00",
     status: true,
-    categoria: "conta",
+    categoria: "1010232324",
   },
   {
     dataVencimento: "18/03/2023",
     despesa: "lanche",
     valor: "127.58",
     status: true,
-    categoria: "alimentação",
+    categoria: "1010232322",
   },
   {
     dataVencimento: "01/03/2023",
     despesa: "viajem",
     valor: "17.58",
     status: false,
-    categoria: "lazer",
+    categoria: "1010232323",
   },
   {
     dataVencimento: "07/08/2022",
     despesa: "compras",
     valor: "79.2",
     status: false,
-    categoria: "alimentação",
+    categoria: "1010232322",
   },
   {
     dataVencimento: "10/10/2022",
     despesa: "cinema",
     valor: "50.0",
     status: true,
-    categoria: "lazer",
+    categoria: "1010232323",
   },
   {
     dataVencimento: "11/03/2023",
     despesa: "luz",
     valor: "150.00",
     status: true,
-    categoria: "conta",
+    categoria: "1010232324",
   },
 ];
 
 //criação de funções
 
+function identificaCategoria (id){
+  let nomecategoria = listaCategorias.filter(categoria => categoria.id == id)
+  return nomecategoria[0].nome
+}
+console.log(identificaCategoria("1010232324"))
 function alternaStatus(i) {
   listaDespesas[i].status = !listaDespesas[i].status;
   imprimeListaDespesas(listaDespesas);
+}
+
+function imprimeListaCategorias(lista) {
+  tabelaCategorias.innerHTML = "";
+  for (let i = 0; i < lista.length; i++) {
+    tabelaCategorias.innerHTML += `<tr>
+    <td>${lista[i].id}</td>
+    <td>${lista[i].nome}</td>
+    <td class="container-btns"><button class="btn">EDITAR</button><button class="btn-vermelho">EXCLUIR</button></td>
+    </tr>`;
+  }
 }
 
 function imprimeListaDespesas(lista) {
@@ -102,6 +133,7 @@ function imprimeListaDespesas(lista) {
     }'>
     <td>${lista[i].dataVencimento}</td>
     <td>${lista[i].despesa}</td>
+    <td>${identificaCategoria(lista[i].categoria)}</td>
     <td>${lista[i].valor}</td>
     <td><button onclick='alternaStatus(${i})'>${
       lista[i].status ? "PAGO" : "PENDENTE"
@@ -127,6 +159,7 @@ function escModal(modal) {
 //Chamadas de funções
 
 imprimeListaDespesas(listaDespesas);
+imprimeListaCategorias(listaCategorias)
 
 headerBtnCategorias.addEventListener("click", function () {
   if (containerCategorias.classList.contains("inativo")) {
@@ -155,3 +188,7 @@ headerBtnDespesas.addEventListener("click", function () {
     escModal(modalAddCategorias);
   });
 });
+
+function gerarIdCategoria (){
+  listaCategorias[listaCategorias.length-1].id
+}
